b2bc0ee8c3bebabc2fb04b1946cc4b716db59fae
syafrudinhl <syafrudintdm@gmail.com>
2023-09-20 02:41:17 +0700

    edit : add table & alert

--- 13_React Hook/praktikum/react-hook/src/page/CreateProduct.jsx
+++ 13_React Hook/praktikum/react-hook/src/page/CreateProduct.jsx
@@ -1,8 +1,13 @@
 import Navbar from "../header/Navbar";
 import Form from "../components/Form";
 import Header from "../header/Header";
+import { useEffect } from "react";
 
 export default function CreateProduct() {
+  useEffect(() => {
+    alert("Welcome");
+  });
+
   return (
     <>
       <Navbar></Navbar>
