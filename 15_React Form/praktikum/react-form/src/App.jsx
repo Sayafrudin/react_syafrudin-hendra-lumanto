d6681184425f6d7876a8d7059f8b2c72bfccb4bb
syafrudinhl <syafrudintdm@gmail.com>
2023-09-27 02:30:05 +0700

    edit : add Validation

--- 15_React Form/praktikum/react-form/src/App.jsx
+++ 15_React Form/praktikum/react-form/src/App.jsx
@@ -1,12 +1,17 @@
-import CreateProduct from "./page/createproduct";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import CreateProduct from "./page/CreateProduct";
+import LandingPage from "./page/LandingPage";
+import DetailProduct from "./page/DetailProduct";
 
 function App() {
   return (
-    <>
-      <div>
-        <CreateProduct></CreateProduct>
-      </div>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/create-product" element={<CreateProduct />} />
+        <Route path="/product/:id" element={<DetailProduct />} />
+      </Routes>
+    </Router>
   );
 }
 
--- 15_React Form/praktikum/react-form/src/components/Form.jsx
+++ 15_React Form/praktikum/react-form/src/components/Form.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { BsFillTrash3Fill, BsFillPencilFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
 import React from "react";
 import InputField from "./InputField";
 import TextArea from "./TextArea";
@@ -9,12 +10,12 @@ import Modal from "./Modal";
 import "../assets/Form.css";
 
 export default function Form() {
-  const [errors, setErrors] = useState("");
-  const [productName, setProductName] = useState("");
+  const [errors, setErrors] = useState({});
   const [array, setArray] = useState([]);
   const [inputData, setInputData] = useState({
     pname: "",
     pprice: "",
+    pimage: "",
     pcategory: "",
     freshness: "",
   });
@@ -24,24 +25,73 @@ export default function Form() {
     { label: "Else", value: "Else" },
   ];
 
-  function data(e) {
-    const value = e.target.value;
-    setProductName(value);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setInputData({
+      ...inputData,
+      [name]: value,
+    });
+  };
 
-    if (value.length === 0) {
-      setErrors("Product Name tidak boleh kosong");
-    } else if (value.length > 10) {
-      setErrors("Product Name tidak boleh melebihi 10 karakter");
-    } else {
-      setErrors("");
+  const { pname, pprice, pimage, pcategory, freshness } = inputData;
+  const addInputData = (e) => {
+    e.preventDefault();
+    const validationErrors = {};
+    if (!inputData.pname) {
+      validationErrors.pname = "Product Name tidak boleh koosng";
+    } else if (!/^[a-zA-Z ]{3,10}$/.test(inputData.pname)) {
+      validationErrors.pname =
+        "Hanya boleh Text, minimal 3 karakter & tidak boleh lebih dari 10 karakter";
     }
-    setInputData({ ...inputData, [e.target.name]: e.target.value });
-  }
-  const { pname, pprice, pcategory, freshness } = inputData;
-  function addInputData() {
-    setArray([...array, { pname, pprice, pcategory, freshness }]);
-    setInputData({ pname: "", pprice: "", pcategory: "", freshness: "" });
-  }
+
+    if (!inputData.pcategory) {
+      validationErrors.pcategory = "Pilih Product Category";
+    } else {
+    }
+
+    if (!inputData.pimage) {
+      validationErrors.pimage = "Product Image tidak boleh kosong";
+    } else {
+    }
+
+    if (!inputData.freshness) {
+      validationErrors.freshness = "Pilih Product Freshness";
+    } else {
+    }
+
+    if (!inputData.pprice) {
+      validationErrors.pprice = "Product Price tidak boleh koosng";
+    } else if (!/^[0-9]{3,}$/.test(inputData.pprice)) {
+      validationErrors.pprice = "Hanya boleh andka & minimal 3 karakter";
+    }
+
+    if (e.target.files && e.target.files[0]) {
+      setInputData(URL.createObjectURL(e.target.files[0]));
+    }
+
+    setErrors(validationErrors);
+
+    if (Object.keys(validationErrors).length === 0) {
+      setArray([
+        ...array,
+        {
+          id: crypto.randomUUID(),
+          pname,
+          pprice,
+          pimage,
+          pcategory,
+          freshness,
+        },
+      ]);
+      setInputData({
+        pname: "",
+        pprice: "",
+        pimage: "",
+        pcategory: "",
+        freshness: "",
+      });
+    }
+  };
 
   function deleteData(i) {
     console.log(i, "ini row yang akan dihapus");
@@ -55,43 +105,60 @@ export default function Form() {
       <form className="row " autoComplete="off">
         <div className="container">
           <div className="row p-3">
-            <InputField
-              label="Product Name"
-              type="text"
-              name="pname"
-              value={inputData.pname}
-              onChange={data}
-            ></InputField>
+            <div className="mb-3">
+              <InputField
+                label="Product Name"
+                type="text"
+                name="pname"
+                onChange={handleChange}
+                value={inputData.pname}
+              ></InputField>
 
-            <span style={{ color: "red" }}>{errors}</span>
+              {errors.pname && (
+                <span style={{ color: "red" }}>{errors.pname}</span>
+              )}
+            </div>
 
             <div className="row">
-              <div className="w-auto mb-4">
+              <div className="w-auto mb-3">
                 <label className="form-label">Product Category</label>
                 <select
-                  onChange={data}
-                  className="form-select"
+                  className="form-select required"
                   name="pcategory"
+                  onChange={handleChange}
                 >
-                  <option type="radio" name="freshness" disabled selected>
+                  <option name="pcategory" value="" disabled selected>
                     Choose...
                   </option>
                   {options.map((option) => (
                     <option value={option.value}>{option.label}</option>
                   ))}
                 </select>
+                {errors.pcategory && (
+                  <span style={{ color: "red" }}>{errors.pcategory}</span>
+                )}
               </div>
             </div>
-            <InputField label="Image of Product" type="file"></InputField>
+            <div className="mb-4">
+              <InputField
+                label="Image of Product"
+                name="pimage"
+                type="file"
+                onChange={handleChange}
+              ></InputField>
+              {errors.pimage && (
+                <span style={{ color: "red" }}>{errors.pimage}</span>
+              )}
+            </div>
             <div className="row">
               <div className="w-auto mb-4">
-                <label className="form-label">Product Category</label>
+                <label className="form-label">Product Freshness</label>
                 <br></br>
                 <input
                   type="radio"
                   name="freshness"
                   value="Brand New"
-                  onChange={data}
+                  onChange={handleChange}
                 />
                 &nbsp;Brand New
                 <br></br>
@@ -99,7 +166,7 @@ export default function Form() {
                   type="radio"
                   name="freshness"
                   value="Second Hand"
-                  onChange={data}
+                  onChange={handleChange}
                 />
                 &nbsp;Second Hand
                 <br></br>
@@ -107,23 +174,29 @@ export default function Form() {
                   type="radio"
                   name="freshness"
                   value="Refurbished"
-                  onChange={data}
+                  onChange={handleChange}
                 />
                 &nbsp;Refurbished
+                {errors.freshness && (
+                  <span style={{ color: "red" }}>{errors.freshness}</span>
+                )}
               </div>
             </div>
             <div className="row">
               <TextArea label="Additional Description"></TextArea>
             </div>
-            <div className="row">
+            <div className="row mb-4">
               <WrapInputField
                 label="Product Price"
-                type="number"
+                type="text"
                 span="$"
                 name="pprice"
+                onChange={handleChange}
                 value={inputData.pprice}
-                onChange={data}
               ></WrapInputField>
+              {errors.pprice && (
+                <span style={{ color: "red" }}>{errors.pprice}</span>
+              )}
             </div>
             <Button
               style={{
@@ -131,7 +204,7 @@ export default function Form() {
                 border: "none",
                 color: "white",
               }}
-              type="button"
+              type="submit"
               onClick={addInputData}
             >
               Create Product
@@ -145,13 +218,13 @@ export default function Form() {
         <table className="table table-hover ">
           <thead className="table-danger ">
             <tr>
-              <th>No</th>
               <th>Id</th>
               <th>Product Name</th>
               <th>Product Category</th>
+              <th>Product Image</th>
               <th>Product Freshness</th>
               <th>Product Price</th>
-              <th>Action</th>
+              <th colSpan={2}>Action</th>
             </tr>
           </thead>
           <tbody>
@@ -159,10 +232,12 @@ export default function Form() {
               array.map((item, i) => {
                 return (
                   <tr key={i}>
-                    <td>{i + 1}</td>
-                    <td>{crypto.randomUUID()}</td>
+                    <td>
+                      <Link to={`/product/${item.id}`}>{item.id}</Link>
+                    </td>
                     <td>{item.pname}</td>
                     <td>{item.pcategory}</td>
+                    <td>{<img src={pimage} alt="preview image" />}</td>
                     <td>{item.freshness}</td>
                     <td>Rp. {item.pprice}</td>
                     <td>
@@ -178,6 +253,8 @@ export default function Form() {
                           style={{ fontSize: "1.2rem", color: "white" }}
                         ></BsFillPencilFill>
                       </Button>
+                    </td>
+                    <td>
                       <Button
                         type="button"
                         toggle="modal"
