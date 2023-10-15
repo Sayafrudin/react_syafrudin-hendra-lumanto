9f633a67882fb90da68640c6d4d2b67021a94937
syafrudinhl <syafrudintdm@gmail.com>
2023-10-15 22:38:32 +0700

    OpenAI Form

---
+++ 24_Basic Model OpenAI dan Prompt Engginer di React/praktikum/react-openai/src/App.jsx
@@ -0,0 +1,11 @@
+import FormAI from "./openai";
+
+function App() {
+  return (
+    <>
+      <FormAI></FormAI>
+    </>
+  );
+}
+
+export default App;
---
+++ 24_Basic Model OpenAI dan Prompt Engginer di React/praktikum/react-openai/src/openai.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import { Form, Button, Container, Row, Col } from "react-bootstrap";
+import OpenAI from "openai";
+import "./styles.css";
+
+export default function FormAI() {
+  const openai = new OpenAI({
+    apiKey: "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",
+    dangerouslyAllowBrowser: true,
+  });
+
+  const [prompt, setPrompt] = useState("");
+  const [result, setResult] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const AICLick = async () => {
+    setLoading(true);
+    try {
+      const resp = await openai.completions.create({
+        model: "gpt-3.5-turbo-instruct",
+        prompt: prompt,
+        temperature: 0.3,
+        max_tokens: 256,
+      });
+      setResult(resp.choices[0].text);
+    } catch (error) {
+      console.log(error);
+    }
+    setLoading(false);
+  };
+
+  return (
+    <div>
+      <Container fluid="md" className="p-5">
+        <Row className="form">
+          <Form.Group className="mb-3" controlId="">
+            <Form.Label>Ask Anything to OpenAI</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={5}
+              name="text_ai"
+              placeholder="What is 2 + 2"
+              onChange={(e) => setPrompt(e.target.value)}
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="">
+            <Form.Label>Result :</Form.Label>
+            <Form.Control as="textarea" rows={5} value={result} />
+          </Form.Group>
+
+          <Button variant="secondary" onClick={AICLick}>
+            Submit
+          </Button>
+
+          <p>{loading ? "AI is Loading...." : ""}</p>
+        </Row>
+      </Container>
+    </div>
+  );
+}
